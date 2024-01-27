ef7994766bc341b2ff0726db0facb422bfd4d817
Thomas Durand <thomas.durand.verjat@gmail.com>
2024-01-27 14:20:24 +0100

    Enabled swift strict concurrency

--- Package.swift
+++ Package.swift
@@ -2,6 +2,8 @@
 
 import PackageDescription
 
+let settings: [SwiftSetting] = [.strictConcurrency, .warnLongExpressionTypeChecking]
+
 let package = Package(
     name: "ScreenshotKit",
     platforms: [.iOS(.v16), .macOS(.v13)],
@@ -14,7 +16,18 @@ let package = Package(
     targets: [
         .target(name: "ScreenshotKit", dependencies: [
             .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
-        ]),
+        ], swiftSettings: settings),
         .testTarget(name: "ScreenshotKitTests", dependencies: ["ScreenshotKit"]),
     ]
 )
+
+extension SwiftSetting {
+    static let strictConcurrency = enableUpcomingFeature("StrictConcurrency")
+    static let warnLongExpressionTypeChecking = unsafeFlags(
+        [
+            "-Xfrontend", "-warn-long-expression-type-checking=100",
+            "-Xfrontend", "-warn-long-function-bodies=100",
+        ],
+        .when(configuration: .debug)
+    )
+}
